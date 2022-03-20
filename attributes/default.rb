384986dc75eddbb63178aec13af5e5ed9cfd3186
Tim Smith <tsmith84@gmail.com>
2022-03-19 19:05:20 -0700

    Add spell check CI job
    Make sure we keep this well written
    
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 
 default['mondoo'].tap do |mondoo|
-  mondoo['registration_token'] = 'changeme'
+  mondoo['registration_token'] = 'change_me'
 
   mondoo['deb']['repo'] = 'https://releases.mondoo.com/debian/'
   mondoo['deb']['gpgkey'] = 'https://releases.mondoo.com/debian/pubkey.gpg'
