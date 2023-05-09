fd19da3b8d1a13dc77bf2c05fbc0b18939952fd1
Tim Smith <tim@mondoo.com>
2023-05-08 17:23:31 -0700

    Add chefspec tests
    Signed-off-by: Tim Smith <tsmith84@gmail.com>

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'chefspec'
+require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.color = true
+  config.formatter = :documentation
+  config.log_level = :error
+  config.platform = 'ubuntu'
+  config.version = '20.04'
+end
