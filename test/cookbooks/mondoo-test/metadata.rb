fa964f7a687e3a9512c26b46a467204ca7740deb
Ben Rockwood <benr@cuddletech.com>
2023-09-20 15:35:11 -0700

    Fix conflict
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- test/cookbooks/mondoo-test/metadata.rb
+++ test/cookbooks/mondoo-test/metadata.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # Copyright:: Mondoo, Inc.
-=======
-# Copyright (c) Mondoo, Inc.
->>>>>>> 8309bcbf980064d3bab9236e47d293ef8d4639bd
 # SPDX-License-Identifier: BUSL-1.1
 
 name 'mondoo-test'
