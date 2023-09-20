153dd623cee87503237f6bd004c776aac79d3ac9
Ben Rockwood <benr@cuddletech.com>
2023-09-20 15:36:49 -0700

    Fix
    Signed-off-by: Ben Rockwood <benr@cuddletech.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 # Copyright:: Mondoo, Inc.
 # SPDX-License-Identifier: BUSL-1.1
-=======
-# Copyright:: 2021-2023 Mondoo, Inc
-# License:: BUSL-1.1
->>>>>>> 8309bcbf980064d3bab9236e47d293ef8d4639bd
 
 
 default['mondoo']['registration_token'] = 'change_me'
